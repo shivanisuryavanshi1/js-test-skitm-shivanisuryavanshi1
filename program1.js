7a788c058cd81061b879c7a11f90a10b775c5707
shivanisuryavanshi1 <shivaniii0612@gmail.com>
2024-01-12 14:39:27 +0530

    submit

--- program1.js
+++ program1.js
@@ -3,9 +3,30 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+   
+        const stackOfInput = [];
+        const mappingInput = { ')': '(', '}': '{', ']': '[' };
     
-};
+        for (let char of s) {
+            if (char in mappingInput) {
+                const topElement = stackOfInput.pop() || '#';
+                if (mappingInput[char] !== topElement) {
+                    return false;
+                }
+            } else {
+                stackOfInput.push(char);
+            }
+        }
+    
+        return stackOfInput.length === 0;
+    }
+    
+    console.log(isValid("()"));       
+    console.log(isValid("()[]{}"));     
+    console.log(isValid("(]"));
+    
+
 
 module.exports = { isValid };
 
-
+          
--- program2.js
+++ program2.js
@@ -3,8 +3,37 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+
+        const inputValues = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        };
     
-};
+        let result = 0;
+    
+        for (let i = 0; i < s.length; i++) {
+            const presentValue = inputValues[s[i]];
+            const nextValue = inputValues[s[i + 1]];
+    
+            if (nextValue > presentValue) {
+                result += nextValue - presentValue;
+                i++; 
+            } else {
+                result += presentValue;
+            }
+        }
+    
+        return result;
+    
+}
+    console.log(romanToInt("III")); 
+    console.log(romanToInt("LVIII")); 
+    console.log(romanToInt("MCMXCIV")); 
 
 
 module.exports={romanToInt}
